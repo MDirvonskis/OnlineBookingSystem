92c27d3dc3a96e3f3980c2f7684a6ba1d69a1cd7
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-05-07 22:42:19 +0100

    Deleted boilerplate from react installed.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
+const port = 5000
 
 app.set("view engine", "ejs")//sets engine as ejs
-
 
 app.get("/", (req,res) => {
     console.log("here")
@@ -14,4 +14,4 @@ const userRouter = require('./routes/users')
 app.use("/users", userRouter)
 //app.user("/post", postRouter)  
 
-app.listen(3000)
+app.listen(port, () => {console.log("Server started on port 5000")})
