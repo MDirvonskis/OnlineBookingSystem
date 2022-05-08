14096c1d28ab3b38dd008b8e327582e253cf0e62
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-05-08 22:21:57 +0100

    Fixed spelling mistake.

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  //let history = useHistory()
+  let navigate = useNavigate();
   return (
-    <div>Home</div>
+    <div>
+      <button onClick={()=> {navigate('/LogIn')}}>LogIn</button>
+      <button onClick={()=> {navigate('/SignUp')}}>SignUp</button>
+    </div>
   )
 }
 
