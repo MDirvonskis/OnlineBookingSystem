62d827f2c11634a96d014399fe1b243ea3412872
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-05-08 22:21:32 +0100

    Created rootes for LogIn, Host, SignUp

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react'
 import Home from "./Pages/Home"
 import LogIn from "./Pages/LogIn"
-import SighUp from "./Pages/SighUp"
+import SighUp from "./Pages/SignUp"
 import errorPg from "./Pages/errorPg"
-
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/LogIn" element={<LogIn />}/>
+          <Route path="/SignUp" element={<SighUp />}/>
+          
+          <Route path="/AdminPanel" element={<LogIn />}/>
+          <Route path="/ClientBooking" element={<LogIn />}/>
+          <Route path="/AdminPanel/DatabaseManagement" element={<LogIn />}/>
         </Routes>
     </Router>
   )
