1e2086293cc69c0892f7a0f9d38552f79a25206b
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-05-08 17:47:54 +0100

    Created a check if admin account and user input match.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState } from 'react'
-import LogInForms from './componets/LogInForm'
+import LogInForm from './componets/LogInForm'
 
 function App() {
   const adminUser = {
@@ -7,24 +7,34 @@ function App() {
     password: "admin123"
   }
 
-  const [user, setUser] = userState({userName: "", email: ""})
-  const [error, setError] = useState = useState("")
+  const [user, setUser] = useState({userName: "", password: ""})
+  const [error, setError] = useState("")
 
-  const LogIn = details =>{
-    console.log(details)
+  const LogIn = details =>
+  {
+    //console.log(details.userName)
+    //console.log(details.password)
+    //console.log(adminUser.userName)
+    //console.log(adminUser.password)
+    if(details.userName === adminUser.userName && details.password === adminUser.password) {
+      console.log("Logged In")
+    }
+    else{
+      console.log("Details do not match")
+    }
   }
   const LogOut = () =>{
     console.log("LogOut")
   }
   return (
     <div className="App">
-      {(user.userName != "") ? (
+      {(user.userName !== "") ? (
         <div className ="Welcome">
         <h2>Welcome, <span>{user.name}</span></h2>
         <button>LogOut</button>
         </div>
       ) : (
-        <LogInForm LogIn={LogIn} error={error} />//Initiate log form
+        <LogInForm LogIn={LogIn} error={error} />//Initiate login form
       )}
     </div>    
   )
