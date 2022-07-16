7f81d95a88f23f6b63fa5b89c48dcdc20214c242
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-16 20:42:45 +0100

    Revert to old branch. - Old branch did not work and woulnt lauch the server

---
+++ app/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+//app.set("view engine", "ejs")//sets engine as ejs
+
+app.get("/", (req,res) => {
+    console.log("here")
+    res.send("ho")
+    //res.render('index' ,{text: 'Word'})
+})
+
+//const userRouter = require('./routes/users')
+//const postRouter = require('./routes/posts')
+//app.use("/users", userRouter)
+//app.user("/post", postRouter)  
+
+app.listen(port, () => {console.log("Server started on port 5000")})
