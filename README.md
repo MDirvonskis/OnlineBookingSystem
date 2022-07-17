4bbad61e32a60adfdf1259871c829f85c1daf97e
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-17 10:31:35 +0100

    Created a function where you delete customer by id

--- README.md
+++ README.md
@@ -37,3 +37,15 @@ Create a new customer with password.
 ```
 curl -v -X POST    http://localhost:5000/addCustomer -H "Content-Type: application/json" -d '{"customerID": "test", "forename": "test", "surname": "test", "email": "test@gmail.com", "password": "test"}'
 ```
+Create a new room
+```
+curl -v -X POST    http://localhost:5000/addRoom -H "Content-Type: application/json" -d '{"roomID": 123, "capacity": "test", "info": "test"}'
+```
+Create a new booking
+```
+curl -v -X POST    http://localhost:5000/addBooking -H "Content-Type: application/json" -d '{"bookingID": 1,"dateOfBooking": "1999-2-5","dateOfStay": "1999-2-5","dateOfDeparture": "1999-2-5","customerID": "test", "roomID": 123}'
+```
+Delete a customer from database by their customerID
+```
+curl -v -X DELETE  http://localhost:5000/remCustomer/admins
+```
--- app/server.js
+++ app/server.js
@@ -18,7 +18,7 @@ app.get("/", (req,res) => {
     //res.render('index' ,{text: 'Word'})
 })
 app.get('/getCustomer/:id', (req,res) => {
-    console.log(1)
+    //console.log(1)
     const id = req.params.id
     db.query(
       'SELECT * FROM customers WHERE CustomerID =  ?',
@@ -31,8 +31,7 @@ app.get('/getCustomer/:id', (req,res) => {
 })
 
 app.post('/addCustomer', (req,res) =>{
-    console.log("here2")
-    console.log(req.body.customerID, req.body.forename, req.body.surname, req.body.email)
+    //console.log(req.body.customerID, req.body.forename, req.body.surname, req.body.email)
     var sql = `INSERT INTO customers(customerID,forename,surname,email)
             VALUES (?, ?, ?, ?)`;
     var sql2 = `INSERT INTO passwords(password,customerID)
@@ -49,6 +48,32 @@ app.post('/addCustomer', (req,res) =>{
         if(err){res.statusCode(400)}
         else{res.statusCode(201)}
     }
+})
+app.post('/addRoom', (req,res) =>{
+    //RoomID int
+    //capacity varchar? need to change database later
+    //console.log(req.body.roomID, req.body.capacity, req.body.info)
+    var sql = `INSERT INTO rooms(roomID,capacity,info)
+            VALUES (?, ?, ?)`;
+    db.query(sql,[req.body.roomID, req.body.capacity, req.body.info]),
+    function(err, results, fields)
+    {
+        if(err){res.statusCode(400)}
+        else{res.statusCode(201)}
+    }
+})
+app.post('/addBooking', (req,res) =>{
+    var sql = `INSERT INTO Bookings(BookingID, dateOfBooking, dateOfStay, dateOfDeparture, customerID, RoomID)
+            VALUES (?, ?, ?, ?, ?,?)`;
+    db.query(sql,[req.body.bookingID,req.body.dateOfBooking,req.body.dateOfStay,req.body.dateOfDeparture,req.body.customerID, req.body.roomID])
+})
+app.delete('/remCustomer/:id', (req,res) => {
+    //console.log(1)
+    const id = req.params.id
+    var sql2 = `DELETE FROM customers WHERE CustomerID =  ?`;
+    var sql = `DELETE FROM passwords WHERE CustomerID = ?`;
+    db.query(sql, [id])
+    db.query(sql2, [id])      
 })
 
 
